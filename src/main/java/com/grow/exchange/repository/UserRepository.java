ab809947ea5647b4cff408d52a88cf5665e44241
Grow1008 <sangamcityprayag@gmail.com>
2022-03-21 16:53:10 +0530

    add redeem coin bug fix

--- src/main/java/com/grow/exchange/repository/UserRepository.java
+++ src/main/java/com/grow/exchange/repository/UserRepository.java
@@ -10,13 +10,13 @@ import com.grow.exchange.entity.UserEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
 	
-	@Query("SELECT p FROM UserEntity p where p.userId=?1 And isActive=1")
+	@Query("SELECT p FROM UserEntity p where p.userId=?1 And isActive=true")
 	public UserEntity findUserByUserId(String userId); 
 	
 	@Query("SELECT p FROM UserEntity p where p.id=?1")
 	public UserEntity findUserById(Long Id); 
 	
-	@Query("SELECT p FROM UserEntity p where p.adminId=?1 And isActive=1")
+	@Query("SELECT p FROM UserEntity p where p.adminId=?1 And isActive=true")
 	public List<UserEntity> findUsersByAdminId(String adminId); 
 	
 	}
